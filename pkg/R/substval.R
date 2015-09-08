88ea2a919b6c46bce79693ee4ea4bb6f49280dea
mark <mark.vanderloo@gmail.com>
2015-09-08 16:27:34 +0200

    added value substitution

---
+++ pkg/R/substval.R
@@ -0,0 +1,30 @@
+#' Substitute a value in a system of linear (in)equations
+#'
+#' @section Details:
+#' A system of the form \code{Ax <= b} can be simplified if one or more of the
+#' \code{x[i]} values is fixed.
+#'
+#'
+#' @param A \code{[numeric]} matrix
+#' @param b \code{[numeric]} vector 
+#' @param variables \code{[numeric|logical|character]} vector of column indices in \code{A}
+#' @param values \code{[numeric]} vecor of values to substitute.
+#'
+#' @return A \code{list} with the following components:
+#' 
+#' \itemize{
+#'   \item{\code{A}: the \code{A} corresponding to the simplified sytem.}
+#'   \item{\code{b}: the constant vector corresponding to the new system}
+#' }
+#'
+#'
+#' @export
+subst_value <- function(A, b, variables, values){
+  if ( is.character(variables) ){
+    variables <- match(variables,colnames(A))
+  }
+
+  b <- as.vector(b - A[,variables,drop=FALSE] %*% values)
+  A <- A[,-variables,drop=FALSE]
+  list(A=A, b=b)
+}
