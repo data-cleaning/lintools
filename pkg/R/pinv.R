65859074c53390a7a473ef55841fed198eacb8c9
mark <mark.vanderloo@gmail.com>
2016-01-08 14:38:17 +0100

    exported pinv

--- pkg/R/pinv.R
+++ pkg/R/pinv.R
@@ -7,7 +7,7 @@
 #' @param A [numeric] matrix
 #' @param eps [numeric] tolerance for determining zero singular values
 #'
-#' @keywords internal
+#' @export
 pinv <- function(A, eps=1e-8){
   L <- svd(A)
   d <- L$d
