ae5301daec63a6a05e5b1eb60da60ffd63b53168
mark <mark.vanderloo@gmail.com>
2016-02-29 11:52:47 +0100

    added unimodularity test, also updated build sequence

---
+++ examples/unimodular.R
@@ -0,0 +1,27 @@
+
+# Totally unimodular matrix, reduces to nothing
+A <- matrix(c(
+ 1,1,0,0,0,
+ -1,0,0,1,0,
+ 0,0,01,1,0,
+ 0,0,0,-1,1),nrow=5)
+is_totally_unimodular(A)
+
+# Totally unimodular matrix, by Heller-Tompson criterium
+A <- matrix(c(
+ 1,1,0,0,
+ 0,0,1,1,
+ 1,0,1,0,
+ 0,1,0,1),nrow=4)
+is_totally_unimodular(A)
+
+# Totally unimodular matrix, by Raghavachani recursive criterium
+A <- matrix(c(
+    1,1,1,
+    1,1,0,
+    1,0,1))
+is_totally_unimodular(A)
+
+
+
+
